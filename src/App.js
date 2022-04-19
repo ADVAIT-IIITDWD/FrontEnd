4146b47b775096be3c90a4cf1276cf68e76f4b5f
Brij Vaghani-iiitdwd <88758778+Brij2003@users.noreply.github.com>
2022-04-19 22:16:30 +0530

    resolved conflict

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import { Route, Routes, BrowserRouter  } from "react-router-dom";
 import './App.css';
 import Form from './components/pages/form';
 import Home from './components/pages/home';
-<<<<<<< HEAD
 import RunQuery from "./components/pages/run_query";
-import Table_select from './components/pages/table_select';
-=======
 import SelectTable from './components/pages/SelectTable';
 import ViewTable from "./components/pages/ViewTable";
->>>>>>> 8f903e5d56b36af6933ce0900242cc1a57a21060
 
 function App() {
   return (
@@ -18,11 +14,8 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/SelectTable" exact element={<SelectTable />} />
         <Route path="/form" exact element={<Form />} />
-<<<<<<< HEAD
         <Route path="/runquery" exact element={<RunQuery />} />
-=======
         <Route path="/ViewTable" exact element={<ViewTable />} />
->>>>>>> 8f903e5d56b36af6933ce0900242cc1a57a21060
       </Routes>
       </BrowserRouter>
     </>
