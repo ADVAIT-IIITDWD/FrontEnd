ff056f56a6e68d9602add8062a8a9a172cba8c68
Brij Vaghani-iiitdwd <88758778+Brij2003@users.noreply.github.com>
2022-04-19 22:18:04 +0530

    resolved conflict

--- src/components/layouts/navbar.js
+++ src/components/layouts/navbar.js
@@ -10,7 +10,6 @@ const Navbar = () => {
                   <div>Vehicle Insurance Database</div>
               </div>
               <ul class="vertical-menu">
-<<<<<<< HEAD
                 <Link to='/runquery'>
                   <li><a class="functn" href="/table_select">Add Data</a></li>
                 </Link>
@@ -23,7 +22,6 @@ const Navbar = () => {
                   <hr/>
                 <Link to='/runquery'>
                   <li><a class="functn" href="/table_select">View Data</a></li>
-=======
                 <Link to='/SelectTable'>
                   <li><a class="functn" href="/SelectTable">Add Data</a></li>
                 </Link>
@@ -36,7 +34,6 @@ const Navbar = () => {
                   <hr/>
                 <Link to='/ViewTable'>
                   <li><a class="functn" href="/ViewTable">View Data</a></li>
->>>>>>> 8f903e5d56b36af6933ce0900242cc1a57a21060
                 </Link>
                   <hr/>
                   <li id="user-info" style={{marginTop: "170%"}}>Team ADVAIT<span>DBMS</span></li>
